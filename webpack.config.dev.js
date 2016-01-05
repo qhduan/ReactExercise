e9bf701af25c2a2904c8be176687369a729d791e
qhduan <mail@qhduan.com>
2016-01-05 21:43:40 +0800

    init project

---
+++ webpack.config.dev.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var path =    require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: [
+    "webpack-hot-middleware/client",
+    path.resolve(__dirname, "src", "js", "app.js")
+  ],
+  module: {
+    loaders: [
+      { // babel
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel"
+      },
+      { // CSS
+        test: /\.css$/,
+        loader: "style!css"
+      },
+      { // SASS
+        test: /\.scss$/,
+        loader: "style!css!sass"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/",
+    filename: "app.js"
+  }
+};
